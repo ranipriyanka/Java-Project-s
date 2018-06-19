7eb36bc06b9518819b5577de64dc1dd0f4151cca
priyanka <pc8813806555@gmail.com>
2018-06-19 15:11:55 +0530

    my files

---
+++ class-8/Substrings.java
@@ -0,0 +1,13 @@
+class Substring{
+public static void main(String[] args){
+String str="xyz";
+SubString(str,str.length());
+}
+public static void main(String str,int s){
+for(int i;i<s;i++){
+for(int j=j+1;j<=a;j++){
+Sysyem.out.println(str.substrings(i,j));
+}
+}
+}
+}
