83c6f27310efb9045ac3d6524d66302890c99ec3
priyanka <pc8813806555@gmail.com>
2018-06-20 11:57:54 +0530

    Abc.text

--- class-9/Abc.java
+++ class-9/Abc.java
@@ -7,18 +7,8 @@
         File f=new File("c:\\users\\HP\\Desktop\\Training\\class-9\\Abc.txt");
 		
 	                 FileInputStream fis=new FileInputStream (f);
-                     FileOutputStream fos=new FileOutputStream(f);
+                    
 	
-          String text="Nothing is imposible when God is on your side.";
-		  
-        byte[] b=text.getBytes();
-		
-        for(byte b1:b)
-		{
-	    fos.write(b1);
-         }
-           fos.flush();
-            fos.close();
 	 
        int i;
 	   
